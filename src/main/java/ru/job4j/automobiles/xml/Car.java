a705c7c8fe2667480015e4a28aa7979f653d098f
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2020-04-21 14:01:13 +0300

    aster

---
+++ src/main/java/ru/job4j/automobiles/xml/Car.java
@@ -0,0 +1,80 @@
+package ru.job4j.automobiles.xml;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(exclude = "owners")
+@ToString(exclude = "owners")
+public class Car {
+    private int id;
+    private String model;
+    private String yearOfRelease;
+    private String bodyType;
+    private Engine engine;
+    private Set<Driver> owners = new HashSet<>(0);
+
+    public Car() {
+    }
+
+    public Car(int id) {
+        this.id = id;
+    }
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(String yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Set<Driver> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(Set<Driver> owners) {
+        this.owners = owners;
+    }
+
+    public void addOwner(Driver driver) {
+        this.owners.add(driver);
+    }
+}
---
+++ src/main/java/ru/job4j/automobiles/xml/Driver.java
@@ -0,0 +1,67 @@
+package ru.job4j.automobiles.xml;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(exclude = "cars")
+@ToString(exclude = "cars")
+public class Driver {
+    private int id;
+    private String name;
+    private String sex;
+    private int age;
+    private Set<Car> cars = new HashSet<>(0);
+
+    public Driver() {
+    }
+
+    public Driver(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Set<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void addCar(Car car) {
+        this.cars.add(car);
+    }
+}
